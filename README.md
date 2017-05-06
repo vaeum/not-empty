aa254dc39ab59c24ac289705fccf15c63574d129
Alexander Anpleenko <vaeum@yahoo.com>
2017-05-06 20:37:22 +0300

    add consts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# not-empty
+# Not empty
 
 ### Small lib
 
@@ -6,6 +6,23 @@
 
 [![NPM version][npm-image]][npm-url]
 [![Downloads][downloads-image]][npm-url]
+
+## Install
+
+```
+npm i -S not-empty
+```
+
+## Usage
+
+```
+import notEmpty from 'not-empty';
+
+notEmpty(0, 'string', true, [1, 2, 4]); // true
+notEmpty(0, '', true, [1, 2, 4]); // false
+notEmpty(0, 'string', true, [], {}); // false
+notEmpty(0, 'string', true, [23, 45], { test: true }); // true
+```
 
 [downloads-image]: https://img.shields.io/npm/dm/not-empty.svg
 [npm-url]: https://www.npmjs.com/package/not-empty
--- index.js
+++ index.js
@@ -1,46 +1,60 @@
+// const ARGUMENTS = 'Arguments';
+// const GLOBAL = 'Global';
+const ARRAY = 'Array';
+const DATE = 'Date';
+const ERROR = 'Error';
+const FUNCTION = 'Function';
+const NULL = 'Null';
+const NUMBER = 'Number';
+const OBJECT = 'Object';
+const REG_EXP = 'RegExp';
+const STRING = 'String';
+const UNDEFINED = 'Undefined';
+const BOOL = 'Boolean';
+
 export const notEmpty = (...arg) => {
-  if(!arg.length) return false;
+  if (!arg.length) return false;
 
   function classof(obj) {
     return Object.prototype.toString.call(obj).slice(8, -1);
   }
 
   let arr = arg.filter((e, i) => {
-    switch (classof(e)){
-      case("String"): return stringFunc(e); break;
-      case("Boolean"): return booleanFunc(e); break;
-      case("Array"): return arrayFunc(e); break;
-      case("Object"): return objectFunc(e); break;
-      case("Number"): return numberFunc(e); break;
-      case("Null"): return false; break;
-      case("Undefined"): return false; break;
+    console.log(classof(e));
+    switch (classof(e)) {
+      case STRING: return stringFunc(e); break;
+      case BOOL: return e; break;
+      case ARRAY: return arrayFunc(e); break;
+      case OBJECT: return objectFunc(e); break;
+      case NUMBER: return numberFunc(e); break;
+      case REG_EXP: return true; break;
+      case ERROR: return true; break;
+      case DATE: return true; break;
+      case FUNCTION: return true; break;
+      case NULL: return false; break;
+      case UNDEFINED: return false; break;
+
       default: return false;
     }
   });
 
   function stringFunc(string) {
-    if(string) return true;
-    return false;
-  }
-
-  function booleanFunc(bool) {
-    if(bool) return true;
+    if (string) return true;
     return false;
   }
 
   function arrayFunc(array) {
-    if(array.length) return true;
+    if (array.length) return true;
     return false;
   }
 
   function objectFunc(object) {
-    if(Object.keys(object).length) return true;
+    if (Object.keys(object).length) return true;
     return false;
   }
 
   function numberFunc(number) {
-    if(isFinite(number)) return true;
-    return false;
+    return isFinite(number);
   }
 
   return arr.length == arg.length ? true : false;
